08f1358f8e0bf3f477351deb4cbd8cca67d8b240
David Lopez <davidlopez.hellin@outlook.com>
2023-07-12 00:19:12 +0200

    Fix release script

--- tools/arcanist.sh
+++ tools/arcanist.sh
@@ -10,11 +10,13 @@ release() {
     project_version=$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "arcanist").version')
     echo "Project version ('Cargo.toml') : ${project_version}"
 
+    commit=false
     install_version=$(sed -n "s/VERSION=\"\([^\"]*\)\"/\1/p" install.sh)
     echo -n "Install version ('install.sh') : ${install_version}"
     if [[ "$project_version" != "$install_version" ]]; then
         echo " -> ${project_version}"
         sed -i "s/VERSION=\".*\"/VERSION=\"$project_version\"/" install.sh
+        commit=true
     else
         echo
     fi
@@ -24,13 +26,25 @@ release() {
     if [[ "$project_version" != "$install_version" ]]; then
         echo " -> ${project_version}"
         sed -i "s/\$VERSION *= *\".*\"/\$VERSION = \"$project_version\"/" install.ps1
+        commit=true
     else
         echo
     fi
 
-    git tag -a "v${project_version}" -m "Arcanist version ${project_version}"
-    echo "Tag v${project_version} created, pushing tag to origin..."
+    if [[ $commit == true ]]; then
+        git add install.sh install.ps1
+        git commit -m 'Bump install versions'
+        echo "Commit with bumped install version created, commit ready to be pushed, press any key to continue..."
+        read -d'' -s -n1
+        git push
+    fi
 
+    git tag -a "v${project_version}" -m "Arcanist version ${project_version}"
+    if [[ $? -gt 0 ]]; then
+        echo "Failed to create tag 'v${project_version}'"
+        exit
+    fi
+    echo "Tag v${project_version} created, pushing tag to origin..."
     echo "Ready to push tag v${project_version}, press any key to continue"
     read -d'' -s -n1
     git push origin "v${project_version}"
